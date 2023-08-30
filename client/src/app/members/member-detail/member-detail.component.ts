e0597eb2c4f61acd100548303252611a1e684a53
Abel <abeltmengistu@outlook.com>
2023-08-30 14:32:15 -0700

    commiting temp changes

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,12 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 import { Member } from 'src/app/_models/member';
 import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-detail',
+  standalone: true,
   templateUrl: './member-detail.component.html',
-  styleUrls: ['./member-detail.component.css']
+  styleUrls: ['./member-detail.component.css'],
+  imports: [CommonModule,TabsModule]
 })
 export class MemberDetailComponent implements OnInit {
   member: Member | undefined;
